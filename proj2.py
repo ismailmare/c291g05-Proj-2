0c88b8667c041ea4ec0698788dacb90b05a24cfc
ismailmare <imare@ualberta.ca>
2015-11-12 20:06:22 -0700

    Update proj2.py

--- proj2.py
+++ proj2.py
@@ -1,1 +1,24 @@
+# Assignment:           Mini Project 1
+# Due Date:             October, 27 2015
+# Name:                 Ismail Mare, Janice Loo, Preyanshu Kumar
+# Unix ID:              imare, jloo, preyansh
+# StudentID:            1388973, 1359624, 1395321
+# Lecture Section:      B1
+# Instructor:           Davood Rafiei
+#---------------------------------------------------------------
+#
 
+#importing necessary libraries
+import cx_Oracle
+import sys
+import datetime
+import math
+import random
+import time
+import string
+
+
+#----------------------------------------------------
+
+def main():
+  return
